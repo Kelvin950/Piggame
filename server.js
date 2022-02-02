84a1970e92e24cfac3c97eaaa9fa7400ae75b1b1
Kelvin Baiden <denlinato@gmail.com>
2022-02-02 18:23:55 +0000

    included a favicon to the application and deleteda try html file

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express =  require("express");
 const app  = express();
+const favicon =  require('serve-favicon');
 const socketio= require("socket.io");
 
 
-
 app.use(express.static(__dirname+"/public"));
+app.use(favicon(path.join(__dirname , 'public' , 'img', 'favicon.ico')));
 
 const expressServer =  app.listen(process.env.PORT || 5000, ()=>{
     console.log("http://localhost:8");
